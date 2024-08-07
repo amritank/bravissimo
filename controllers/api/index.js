0dedcc1dbf9d93cfee45774572272ae912e77314
amritank <amrita.sasi.nair@gmail.com>
2024-08-07 16:13:02 -0700

    added delete route logic and error checks

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,5 +1,8 @@
 // Import just the router express
 const router = require('express').Router();
 
+// import the appreciation routes
+const appreciationRoutes = require("./appreciation.js");
+router.use("/appreciation", appreciationRoutes);
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -6,4 +6,5 @@ const apiRoutes = require('./api');
 // When a request is made to the /api route, it will be directed to the index.js in the 'api' folder.
 router.use('/api', apiRoutes);
 
+router.use('*', apiRoutes);
 module.exports = router;
