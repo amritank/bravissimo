c73855c01cefb0f90293d13b74676360bd2001ca
amritank <159839663+amritank@users.noreply.github.com>
2024-08-08 10:47:35 -0700

    Update index.js

--- controllers/index.js
+++ controllers/index.js
@@ -4,6 +4,7 @@ const router = require('express').Router();
 // Import the index.js from 'api' folder
 const apiRoutes = require('./api');
 
+// you can ignore this logic as i did it just for testing. you can follow what you have. 
 router.use('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 // When a request is made to the /api route, it will be directed to the index.js in the 'api' folder.
 router.use('/api', apiRoutes);
