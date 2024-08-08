ca717e5def25d3b157f362d10a314c4fbd71bc17
amritank <amrita.sasi.nair@gmail.com>
2024-08-08 10:45:16 -0700

    landing page ui

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
+const path = require('path');
 // Import just the router express
 const router = require('express').Router();
 // Import the index.js from 'api' folder
 const apiRoutes = require('./api');
 
+router.use('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 // When a request is made to the /api route, it will be directed to the index.js in the 'api' folder.
 router.use('/api', apiRoutes);
 
