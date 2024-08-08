cdafa27960180caef302177e5d92ee2ca42c1dfc
Johannes Stoppler <johannesstoppler@Johanness-MBP.attlocal.net>
2024-08-08 13:54:41 -0700

    update

--- controllers/index.js
+++ controllers/index.js
@@ -5,7 +5,7 @@ const router = require('express').Router();
 const htmlRoutes = require ("./htmlRoutes");
 router.use ("/",htmlRoutes);
 // When a request is made to the /api route, it will be directed to the index.js in the 'api' folder.
-//router.use('/api', apiRoutes);
+router.use('/api', apiRoutes);
 
 // TODO: Implement 404 route for all other paths.
 module.exports = router;
